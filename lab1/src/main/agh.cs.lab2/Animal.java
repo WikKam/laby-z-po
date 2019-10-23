02d0273c1888e2b038bebd5583f68389db759d5d
WikKam <kamykikamienie@gmail.com>
2019-10-23 18:51:17 +0200

    test

--- lab1/src/main/agh.cs.lab2/Animal.java
+++ lab1/src/main/agh.cs.lab2/Animal.java
@@ -1,8 +1,11 @@
 package agh.cs.lab2;
 
+import static agh.cs.lab2.MapDirection.*;
+
 public class Animal {
         private Vector2d position = new Vector2d(2,2);
-        private MapDirection direction = MapDirection.NORTH;
+        private MapDirection direction = NORTH;
+        private IWorldMap map;
         public String toString(){
             return position.toString() + " " + direction.toString();
         }
@@ -17,20 +20,20 @@ public class Animal {
                 case FORWARD:
                     switch(direction){
                         case NORTH:
-                            if(position.y<4)
-                            this.position = new Vector2d(position.x,position.y+1);
+                            if(map.canMoveTo(NORTH.toUnitVector().add(position)))
+                                this.position = this.position.add(NORTH.toUnitVector());
                             break;
                         case EAST:
-                            if(position.x<4)
-                            this.position = new Vector2d(position.x+1,position.y);
+                            if(map.canMoveTo(EAST.toUnitVector().add(position)))
+                                this.position = this.position.add(NORTH.toUnitVector());
                             break;
                         case SOUTH:
-                            if(position.y>0)
-                            this.position = new Vector2d(position.x,position.y-1);
+                            if(map.canMoveTo(SOUTH.toUnitVector().add(position)))
+                                this.position = this.position.add(SOUTH.toUnitVector());
                             break;
                         case WEST:
-                            if(position.x>0)
-                            this.position = new Vector2d(position.x-1,position.y);
+                            if(map.canMoveTo(WEST.toUnitVector().add(position)))
+                                this.position = this.position.add(WEST.toUnitVector());
                             break;
                     }
                     break;
@@ -62,5 +65,26 @@ public class Animal {
         }
         public MapDirection getDirection(){
             return this.direction;
+        }
+        public String getShortString(){
+            switch(direction) {
+                case NORTH:
+                    return "N";
+                case SOUTH:
+                    return "S";
+                case WEST:
+                    return "W";
+                case EAST:
+                    return "E";
+                default:
+                    return null;
+            }
+        }
+        public Animal(IWorldMap map){
+            this.map = map;
+        }
+         public Animal(IWorldMap map, Vector2d position){
+        this.map = map;
+        this.position = position;
         }
 }
---
+++ lab1/src/main/agh.cs.lab2/RectangularMap.java
@@ -0,0 +1,47 @@
+package agh.cs.lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RectangularMap implements IWorldMap {
+    private int width;
+    private int height;
+    List<Animal> animals = new ArrayList<>();
+    public RectangularMap(int width, int height){
+    this.width = width;
+    this.height = height;
+    }
+    @Override
+    public boolean canMoveTo(Vector2d position) {
+       if(position.x>width||position.x<0||position.y>height||position.y<0) return false;
+       return true;
+    }
+
+    @Override
+    public boolean place(Animal animal) {
+        if(isOccupied(animal.getPosition()))return false;
+        animals.add(animal);
+        return true;
+    }
+
+    @Override
+    public void run(MoveDirection[] directions) {
+
+    }
+
+    @Override
+    public boolean isOccupied(Vector2d position) {
+        for(Animal animal : animals){
+            if(animal.getPosition().equals(position))return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object objectAt(Vector2d position) {
+       for(Animal animal : animals){
+           if(animal.getPosition().equals(position))return animal;
+       }
+        return null;
+    }
+}
